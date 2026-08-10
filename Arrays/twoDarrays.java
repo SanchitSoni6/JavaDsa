f72713f6f820e30a65f7c4b6cc51ab87edb673c2
Sanchit Soni <sanchitsoni06@gmail.com>
2026-08-10 10:00:39 +0530

    did 2darray question

---
+++ Arrays/twoDarrays.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class twoDarrays{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int [][] twod= new int[rows][cols];
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<cols;j++){
+                twod[i][j]=sc.nextInt();
+            }
+         }
+        System.out.println("Enter the element to find indices");
+        int x = sc.nextInt();
+        for (int i=0;i<rows;i++){
+            for (int j=0;j<cols;j++){
+                if (twod[i][j]==x){
+                    System.out.println("Element found at index: "+i+","+j);
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
